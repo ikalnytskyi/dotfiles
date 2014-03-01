c39acb4dd27e0c1fb3083c435e59567337651331
Igor Kalnitsky <igor@kalnitsky.org>
2014-03-02 00:21:44 +0200

    Add vcprompt.sh bootstrap script

---
+++ _bootstraps/vcprompt.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+#
+# Checkout latest version of `vcprompt`, compile it and
+# install into local binary dir: $DOTFILES_DIR/bin/.bin
+#
+
+HG_REPO="https://bitbucket.org/gward/vcprompt"
+
+DOTFILES_DIR=$(dirname `readlink -f $0`)/..
+INSTALL_DIR=$DOTFILES_DIR/bin/.bin
+
+
+# check for hg
+if [ -x hg ]; then
+    echo "ABORTED: please install mercurial"
+    exit 1
+fi
+
+# check for autoconf
+if [ -x autoconf ]; then
+    echo "ABORTED: please install autoconf"
+    exit 1
+fi
+
+
+# get sources
+temp_dir=`mktemp -d`
+hg clone $HG_REPO $temp_dir
+cd $temp_dir
+
+# compile
+autoconf
+./configure
+make
+
+# install
+cp vcprompt $INSTALL_DIR/
