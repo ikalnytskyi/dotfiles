3dcd7a9dc14c8422f4c87147d41eb1150f181c97
Igor Kalnitsky <igor@kalnitsky.org>
2014-06-09 01:42:30 +0300

    Change vcprompt bootstrap

--- _bootstraps/vcprompt.sh
+++ _bootstraps/vcprompt.sh
@@ -5,33 +5,42 @@
 # install into local binary dir: $DOTFILES_DIR/bin/.bin
 #
 
-HG_REPO="https://bitbucket.org/gward/vcprompt"
+SCRIPT_NAME=`basename $0`
+SCRIPT_DIR=`dirname $0`
 
-DOTFILES_DIR=$(dirname `readlink -f $0`)/..
-INSTALL_DIR=$DOTFILES_DIR/bin/.bin
+DOTFILES_DIR="$SCRIPT_DIR/.."
+INSTALL_DIR="$DOTFILES_DIR/bin/.bin"
+
+HG_REPO="https://bitbucket.org/gward/vcprompt"
 
 
 # check for hg
 if ! which hg >/dev/null; then
-    echo "ABORTED: please install mercurial"
-    exit 1
+  echo "ABORTED: please install mercurial"
+  exit 1
 fi
 
 # check for autoconf
 if ! which autoconf >/dev/null; then
-    echo "ABORTED: please install autoconf"
-    exit 1
+  echo "ABORTED: please install autoconf"
+  exit 1
 fi
 
+
 # get sources
-temp_dir=`mktemp -d`
+temp_dir=`mktemp -d /tmp/${SCRIPT_NAME}.XXX`
 hg clone $HG_REPO $temp_dir
-cd $temp_dir
+
 
 # compile
-autoconf
-./configure
-make
+pushd $temp_dir
+  autoconf
+  ./configure
+  make
+popd
+
 
 # install
-cp vcprompt $INSTALL_DIR/
+echo "\nInstalling binary to $INSTALL_DIR"
+mkdir -p "$INSTALL_DIR"
+cp "$temp_dir/vcprompt" "$INSTALL_DIR/"
