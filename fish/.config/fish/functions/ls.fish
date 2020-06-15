0c6e85e6f8bce420c72ee556f938349773ded7e2
Ihor Kalnytskyi <ihor@kalnytskyi.com>
2020-06-16 01:04:43 +0300

    fish: ls --group-directories-first by default

--- fish/.config/fish/functions/ls.fish
+++ fish/.config/fish/functions/ls.fish
@@ -1,3 +1,3 @@
-function ls --wraps='ls --color=auto' --description 'alias ls=ls --color=auto'
-  command ls --color=auto $argv;
+function ls --wraps='ls --color=auto --group-directories-first' --description 'alias ls=ls --color=auto --group-directories-first'
+  command ls --color=auto --group-directories-first $argv;
 end
