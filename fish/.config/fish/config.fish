4fac5c1990bdac2b0ef176bbab3f6ce9b8531c44
Ihor Kalnytskyi <ihor@kalnytskyi.com>
2023-01-06 19:24:51 +0200

    fish: stylistic updates in the config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,39 +1,23 @@
 set fish_greeting
 
-#
-# ENVIRONMENT VARIABLES
-#
+set --export --global VISUAL nvim             # promote editor of choice
+set --export --global EDITOR nvim             # promote editor of choice
+set --export --global BROWSER firefox         # promote browser of choice
+set --export --global TERMINAL foot           # promote terminal of choice
+set --export --global CLICOLOR 1              # turn on colors for some BSD tools
+set --export --global GPG_TTY (tty)           # setup tty for gpg pinetry
 
-set -xp PATH ~/.local/bin            # executables installed by pip/pipx
-set -xp PATH ~/.cargo/bin            # executables installed by cargo
+fish_add_path --global ~/.local/bin           # executables installed by pip/pipx
+fish_add_path --global ~/.cargo/bin           # executables installed by cargo
 
-set -xg EDITOR nvim                  # promote editor of choice
-set -xg BROWSER firefox              # promote browser of choice
-set -xg TERMINAL foot                # promote terminal of choice
-set -xg PYTHONDONTWRITEBYTECODE 1    # do not produce .pyc/.pyo files
-set -xg CLICOLOR 1                   # turn on colors for some BSD tools
-set -xg GPG_TTY (tty)                # setup tty for gpg2's pinetry
 
-if type -q clang
-  set -xg CC clang                   # use clang as default C compiler
-end
-
-if type -q clang++
-  set -xg CXX clang++                # use clang as default C++ compiler
-end
-
-#
-# SETUP PROMPT WITH BLACKJACK AND HOOKERS
-#
-
+# Starship is the minimal, fast, and  customizable prompt for any shell.
 if status is-interactive; and type -q starship
   starship init fish | source
 end
 
-#
-# SOURCE EXTRA CONFIGURATIONS
-#
 
+# Source per-host configurations as well as localhost overrides.
 if status is-interactive
   for name in config.{$hostname,local}.fish
     if test -e $__fish_config_dir/$name
